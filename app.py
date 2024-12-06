51276d087befbb2fefab7672c2c5978a6458f690
João Vitor Mallet Malheiros <83250409+jvmallet@users.noreply.github.com>
2024-12-05 22:04:16 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,12 @@
 from flask import Flask
 from app.routes import bp
+from app.utils import hash_index  
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+gcs_credentials = os.getenv("GOOGLE_APPLICATION_CREDENTIALS")
+
 
 app = Flask(__name__)
 app.register_blueprint(bp)
